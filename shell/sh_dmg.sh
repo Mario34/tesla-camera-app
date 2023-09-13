6c542699798c9c31eae38c41952d03d25194db7a
azhon <958460248@qq.com>
2023-09-13 11:43:33 +0800

    fix: 优化脚本

--- shell/sh_dmg.sh
+++ shell/sh_dmg.sh
@@ -6,5 +6,6 @@ flutter build macos --release
 
 create-dmg \
   --volname "TeslaCamera" \
+  --app-drop-link 600 185 \
   "TeslaCamera.dmg" \
   "/Users/azhon/office/FlutterProjects/tesla_camera/build/macos/Build/Products/Release/Tesla Camera.app"
